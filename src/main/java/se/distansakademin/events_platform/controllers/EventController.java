11c2c3e49b974f3877bf3e4a63928905d3ebe767
hading <hading@gmail.com>
2024-12-17 16:43:28 +0100

    Test commit

--- src/main/java/se/distansakademin/events_platform/controllers/EventController.java
+++ src/main/java/se/distansakademin/events_platform/controllers/EventController.java
@@ -1,14 +1,68 @@
 package se.distansakademin.events_platform.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import se.distansakademin.events_platform.models.Event;
+import se.distansakademin.events_platform.services.EventService;
+
+import java.util.List;
 
 @Controller
 public class EventController {
 
-    @GetMapping("/events")
-    public String getEventsHome() {
-        return "events";
+    @Autowired
+    private EventService service;
 
+
+    @GetMapping("/events")
+    public String getEventsHome(Model model) {
+        List<Event> allEvents = service.getAllEvents();
+        model.addAttribute("events", allEvents);
+        return "event-list-view";
     }
+
+    @PostMapping( "/seed-events")
+    public String getSeedEvents() {
+        service.seedInitialEvents();
+        return "redirect:/events";
+    }
+
+    @PostMapping("/events")
+    public String postSaveEvent(@ModelAttribute Event event){
+        service.saveEvent(event);
+        return "redirect:/events";
+    }
+
+    @GetMapping("/events/{id}")
+    public String getEvent(@PathVariable Long id, Model model) {
+        Event event = service.getEventById(id);
+        model.addAttribute("event", event);
+        return "event-detail";
+    }
+
+    @PostMapping("/events/delete")
+    public String postDeleteEvent(@ModelAttribute Event event){
+        service.deleteEvent(event);
+        return "redirect:/events";
+    }
+
+    @GetMapping("/events/update/{id}")
+    public String getUpdateEvent(@PathVariable Long id, Model model) {
+        Event event = service.getEventById(id);
+        model.addAttribute("event", event);
+        return "event-detail";
+    }
+
+    @PostMapping("/events/update")
+    public String postUpdateEvent(@ModelAttribute Event event){
+        service.saveEvent(event);
+        return "redirect:/events";
+    }
+
+
 }
--- src/main/java/se/distansakademin/events_platform/services/EventService.java
+++ src/main/java/se/distansakademin/events_platform/services/EventService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import se.distansakademin.events_platform.models.Event;
 import se.distansakademin.events_platform.repositories.EventRepository;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -12,6 +13,8 @@ public class EventService {
 
     @Autowired
     private EventRepository repository;
+    @Autowired
+    private EventRepository eventRepository;
 
     public List<Event> getAllEvents(){
         return repository.findAll();
@@ -19,5 +22,26 @@ public class EventService {
 
     public void saveEvent(Event event){
         repository.save(event);
+    }
+    public void deleteEvent(Event event){
+        repository.delete(event);
+    }
+    public Event getEventById(Long id){
+        return repository.findById(id).orElse(null);
+    }
+
+    public void seedInitialEvents(){
+
+
+        long count = eventRepository.count();
+
+        if(count == 0){
+            List<Event> initialEvents = Arrays.asList(
+                    new Event("Lucia ", "2024-12-13"),
+                    new Event("Christmas", "2024-12-24"),
+                    new Event("New Year", "2024-12-31")
+            );
+            eventRepository.saveAll(initialEvents);
+        }
     }
 }
